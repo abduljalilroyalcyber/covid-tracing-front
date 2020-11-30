0faeb0eef54d204003cf545315628daa15ee1fff
ajsoomro <abdul.jalil@royalcyber.com>
2020-11-30 17:08:51 +0500

    adding code

---
+++ src/main/java/com/covid/front/CovidTracingFrontApplication.java
@@ -0,0 +1,13 @@
+package com.covid.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidTracingFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidTracingFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/covid/front/CovidTracingFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covid.front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTracingFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
